ebcc67e4cdf0fe8ef23e40d0818c947d59e921ca
Argjent <argjent.larv@gmail.com>
2025-02-25 17:05:40 +0100

    Fixed issue in App.js with the ThemeSettings

--- src/App.js
+++ src/App.js
@@ -51,7 +51,7 @@ const App = () => {
                 </div>
 
             <div>
-                    {themeSettings & <ThemeSettings /> }
+                    {themeSettings && <ThemeSettings /> }
 
                 <Routes>
                     {/* Dashboard */}
